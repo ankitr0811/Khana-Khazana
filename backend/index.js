8cef18be629dfb5228cb35dcb30d50899b72bb9f
Ankit Raj <ankitr0811@gmail.com>
2026-07-16 03:27:45 +0530

    Fix CORS for production

--- backend/index.js
+++ backend/index.js
@@ -10,29 +10,19 @@ require("./db")(function (err, data, CatData) {
   global.foodData = data;
   global.foodCategory = CatData;
 });
-
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://khana-khazana-frontend-lspx.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 
